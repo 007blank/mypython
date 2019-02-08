27d2cff3c91a13f33962a057dfe15d73b0c5c73c
007blank <munahata2@gmail.com>
2019-02-08 11:06:29 +0530

    hello

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    import array
+    arr=[]
+    a,b=0,1
+    sum=0
+    while (n>0):
+        arr.append(a)
+        a,b=b,a+b
+        '''arr.append(b)'''
+        n-=1
+    return arr
+no=int(input("enter the value of n for the fibonacci series:"))
+a=fibonacci(no)
+print(a)
